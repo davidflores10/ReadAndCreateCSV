a61fdb8e8511222960a00b0e8699b630b5802486
davidflores10 <davidflores10.df@gmail.com>
2018-10-19 18:00:15 +0200

    Create save CSV File

--- index.py
+++ index.py
@@ -36,39 +36,29 @@ for l in lines:  # Separate the lines in four arrays
             segundaPlantaArray.append(l)
 
 
-with open("sotano.csv", 'w') as myFile:
-    wr = csv.writer(myFile, quoting=csv.QUOTE_ALL)
-    wr.writerow(fields)  # write the first line of the CSV
+def save_in_CSV(CSVName, array):
+    with open(CSVName, 'w') as myFile:
+        wr = csv.writer(myFile, quoting=csv.QUOTE_ALL)
+        wr.writerow(fields)  # write the first line of the CSV
 
-    for s in sotanoArray:  # Loop the rest of lines and write in a CSV file
-        personFields = s.split(",")
-        personFields.pop()  # remove the last element of the array (the floor)
-        wr.writerow(personFields)
+        for p2 in array:  # Loop the rest of lines and write in a CSV file
+            personFields = p2.split(",")
+            personFields.pop()  # remove the last element of the array (the floor)
+            wr.writerow(personFields)
 
 
-with open("plantaBaja.csv", 'w') as myFile:
-    wr = csv.writer(myFile, quoting=csv.QUOTE_ALL)
-    wr.writerow(fields)  # write the first line of the CSV
 
-    for pb in plantaBajaArray:  # Loop the rest of lines and write in a CSV file
-        personFields = s.split(",")
-        personFields.pop()  # remove the last element of the array (the floor)
-        wr.writerow(personFields)
+save_in_CSV("sotano.csv", sotanoArray)
 
-with open("primeraPlanta.csv", 'w') as myFile:
-    wr = csv.writer(myFile, quoting=csv.QUOTE_ALL)
-    wr.writerow(fields)  # write the first line of the CSV
+save_in_CSV("plantaBaja.csv", plantaBajaArray)
 
-    for p1 in primeraPlantaArray:  # Loop the rest of lines and write in a CSV file
-        personFields = p1.split(",")
-        personFields.pop()  # remove the last element of the array (the floor)
-        wr.writerow(personFields)
+save_in_CSV("primeraPlanta.csv", primeraPlantaArray)
 
-with open("segundaPlanta.csv", 'w') as myFile:
-    wr = csv.writer(myFile, quoting=csv.QUOTE_ALL)
-    wr.writerow(fields)  # write the first line of the CSV
+save_in_CSV("segundaPlanta.csv", segundaPlantaArray)
 
-    for p2 in segundaPlantaArray:  # Loop the rest of lines and write in a CSV file
-        personFields = p2.split(",")
-        personFields.pop() # remove the last element of the array (the floor)
-        wr.writerow(personFields)
+
+def orderArray():
+    return
+
+
+
